b850c60b74ca1231f0934a061c4ac38bd6cbab70
zhouhong0607 <278111013@qq.com>
2016-04-15 18:17:22 +0800

    1

---
+++ ACM/src/leetcode236/Solution.java
@@ -0,0 +1,22 @@
+package leetcode236;
+
+public class Solution
+{
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+	{
+
+	}
+
+	public class TreeNode
+	{
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+
+}

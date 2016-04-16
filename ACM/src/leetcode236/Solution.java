1486e2020e64907eea2e342d4547441dcb3ace6f
KOSS <278111013@qq.com>
2016-04-16 17:29:00 +0800

    leetcode（LCA） 236

--- ACM/src/leetcode236/Solution.java
+++ ACM/src/leetcode236/Solution.java
@@ -2,10 +2,16 @@ package leetcode236;
 
 public class Solution
 {
-	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
-	{
-
-	}
+	 public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+	        if(root==null) return null;
+	        if(root==p) return p;
+	        if(root== q) return q;
+	        TreeNode left=lowestCommonAncestor(root.left,p,q);
+	        TreeNode right=lowestCommonAncestor(root.right,p,q);
+	        if(left!=null&&right!=null) return root;
+	        return left==null?right:left;
+	        
+	    }
 
 	public class TreeNode
 	{

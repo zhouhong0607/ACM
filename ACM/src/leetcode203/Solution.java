98315008ea61c979a3bff661ec66806f45e082dc
zhouhong0607 <278111013@qq.com>
2016-05-09 21:45:54 +0800

    leetcode237 (203未做)

---
+++ ACM/src/leetcode203/Solution.java
@@ -0,0 +1,21 @@
+package leetcode203;
+
+public class Solution
+{
+	public ListNode removeElements(ListNode head, int val)
+	{
+		
+	}
+
+	public class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x)
+		{
+			val = x;
+		}
+	}
+
+}
---
+++ ACM/src/leetcode237/Solution.java
@@ -0,0 +1,22 @@
+package leetcode237;
+
+public class Solution
+{
+	public void deleteNode(ListNode node)
+	{
+		node.val=node.next.val;
+		node.next=node.next.next;
+	}
+
+	public class ListNode
+	{
+		int val;
+		ListNode next;
+
+		ListNode(int x)
+		{
+			val = x;
+		}
+	}
+
+}

e456e99b9f7361401ac0532280f63fe4258091f7
KOSS <278111013@qq.com>
2016-05-11 19:34:54 +0800

    test

---
+++ ACM/src/test/Main.java
@@ -0,0 +1,15 @@
+package test;
+
+
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		java.util.Random r = new java.util.Random();
+	
+			System.out.println(r.nextInt(10)+1);
+
+	}
+
+}

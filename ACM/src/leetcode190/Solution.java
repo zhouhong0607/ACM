7fb2e498bfe2b851d3c52c6d803a0effdb1bd0eb
KOSS <278111013@qq.com>
2016-11-30 11:33:32 +0800

    leetcode 190 371 405(未完成)

--- ACM/src/leetcode190/Solution.java
+++ ACM/src/leetcode190/Solution.java
@@ -1,9 +1,22 @@
 package leetcode190;
 
+import java.security.interfaces.RSAKey;
+
 public class Solution
 {
+
 	public int reverseBits(int n)
 	{
-		
+		int res=0;
+		for(int i=0;i<32;i++)
+		{
+			res<<=1;
+			if((n&1)==1)
+				res++;
+			n>>=1;
+			
+		}
+		return res;
 	}
+
 }
---
+++ ACM/src/leetcode405/Solution.java
@@ -0,0 +1,9 @@
+package leetcode405;
+
+public class Solution
+{
+	public String toHex(int num)
+	{
+
+	}
+}
